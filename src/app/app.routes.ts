0c976a6b3c4787f6a3d7a660e43e178722f86292
Alexander Engl <alexanderengl73@gmail.com>
2023-11-22 13:01:50 +0100

    Add main routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DashboardComponent} from "./main/dashboard/dashboard.component";
+import {ProjectComponent} from "./main/project/project.component";
+import {TaskComponent} from "./main/task/task.component";
+import {CalendarComponent} from "./main/calendar/calendar.component";
+import {ReportComponent} from "./main/report/report.component";
+import {SettingsComponent} from "./main/settings/settings.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    title: "Dashboard"
+  },
+  {
+    path: "projects",
+    component: ProjectComponent,
+    title: "Projects"
+  },
+
+  {
+    path: "tasks",
+    component: TaskComponent,
+    title: "Tasks"
+  },
+  {
+    path: "calendar",
+    component: CalendarComponent,
+    title: "Calendar"
+  },
+  {
+    path: "report",
+    component: ReportComponent,
+    title: "Report"
+  },
+  {
+    path: "settings",
+    component: SettingsComponent,
+    title: "Settings"
+  }
+];
