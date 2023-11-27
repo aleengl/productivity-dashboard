f81a3752598dfdec742713d0b6f39142ff42d7c3
Alexander Engl <alexanderengl73@gmail.com>
2023-11-27 11:15:22 +0100

    Add date component

---
+++ src/app/main/dashboard/header/date/date.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-date',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './date.component.html',
+  styleUrl: './date.component.scss'
+})
+export class DateComponent {
+
+}
