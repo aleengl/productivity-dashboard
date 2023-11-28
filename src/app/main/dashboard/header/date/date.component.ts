ddbe9bc8d590ca4dc1b87f03b14a3e4daf9bfa75
Alexander Engl <alexanderengl73@gmail.com>
2023-11-28 11:33:22 +0100

    Add pipe function for dates
    The pipe function converts the current Date into a predefined format

--- src/app/main/dashboard/header/date/date.component.ts
+++ src/app/main/dashboard/header/date/date.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-date',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DatePipe],
   templateUrl: './date.component.html',
-  styleUrl: './date.component.scss'
+  styleUrl: './date.component.scss',
 })
 export class DateComponent {
+  currentDate;
 
+  constructor() {
+    this.currentDate = new Date();
+  }
 }
